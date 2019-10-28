5dc9f271dd6bc8bdce0415d618d3c95b29840599
kolumr <kolumronnie@gmail.com>
2019-10-28 09:39:52 +0300

    Validate date field

--- js/myScript.js
+++ js/myScript.js
@@ -2,10 +2,10 @@ function kana(){
   var bdate = document.getElementById("db").value;
   var rdate = new Date(bdate);
   var day = rdate.getDay();
-  if (bdate == " ")
+  if (bdate === "")
   {
     alert("Please select your date of birth")
-  }
+  }else{
   var nradio = document.getElementsByName("Gender");
   var present = 1;
   for (var i = 0, length = nradio.length; i < length; i++)
@@ -30,5 +30,7 @@ function kana(){
   {
     alert("Please select Gender")
   }
+  r//eturn console
+}
 }
 
